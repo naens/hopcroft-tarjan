1f67a9b9aab6f92c95aa5c60dd264a94f9f8bc9b
naens <naens@gmx.com>
2019-02-18 12:31:11 +0100

    implement frond / tree-edgs dfs

--- planar.pl
+++ planar.pl
@@ -5,26 +5,26 @@ show_dot(DotString):-
         shell('dot -Tpng /tmp/swipl_g.dot -o /tmp/swipl_g.png'),
         shell('feh /tmp/swipl_g.png').
 
-test:-
-        Edges = [edge(a,d),edge(a,b),edge(b,c),edge(b,d), edge(c,d)],
-        edges_to_dict(Edges,GraphDict),
-        write(GraphDict).
-%%        simple_dot(First,DotString),
-%%        show_dot(DotString).
+nonmember(_,[]).
+nonmember(X,[H|T]):-
+        X \= H,
+        nonmember(X,T).
 
 two_directions_edges([edge(A,B)|T],[edge(A,B),edge(B,A)|T2]):-
         two_directions_edges(T,T2).
 two_directions_edges([],[]).
 
-compareEdges(edge(From1,_),edge(From2,_)):-
+compare_edges(edge(From1,_),edge(From2,_)):-
         atom_greater(From2,From1).
-compareEdges(edge(From,To1),edge(From,To2)):-
+compare_edges(edge(From,To1),edge(From,To2)):-
         atom_greater(To2,To1).
 
-edges_to_dict(Edges,Dict):-
+edges_to_dict(Edges,GraphDict):-
         two_directions_edges(Edges,Edges2),
-        merge_sort(Edges2,compareEdges,SortedEdges),
-        Dict = SortedEdges.
+        merge_sort(Edges2,compare_edges,SortedEdges),
+        GraphIn = graph_dict{},
+        SortedEdges = [edge(From,To)|Tail],
+        insert_edges(Tail,From,[To],GraphIn,GraphDict).
 
 intlist_greater([H1|_],[H2|_]):-
         H1 > H2,!.
@@ -61,3 +61,57 @@ merge_lists(L1,L2,Comparator,[H1|T3]):-
 merge_lists(L1,L2,Comparator,[H2|T3]):-
         L2 = [H2|T2],
         merge_lists(L1,T2,Comparator,T3).
+
+%% insert_edges: while creating a new graph, should be called on an empty
+%%   graph.  Inserts edges from a sorted list into the graph dictionary.
+%%   Checks for doubles.  Insert each vertex as an adjacency list.
+%%   Each edge should be represented by edge(A,B) and edge(B,A0), which is
+%%   accomplished by two_directions_edges(EdgesIn,EdgesOut).
+insert_edges([edge(From,To)|Tail],CFrom,CList,GraphIn,GraphOut):-
+        From \= CFrom,!,
+        term_to_atom(adj-CFrom,Key),
+        Graph1 = GraphIn.put(Key,CList),
+        insert_edges(Tail,From,[To],Graph1,GraphOut).
+insert_edges([edge(From,To)|Tail],From,CList,GraphIn,GraphOut):-
+        CList = [CTo|_],
+        To \= CTo,!,
+        insert_edges(Tail,From,[To|CList],GraphIn,GraphOut).
+insert_edges([edge(From,To)|Tail],From,CList,GraphIn,GraphOut):-
+        CList = [To|_],!,
+        insert_edge(Tail,From,CList,GraphIn,GraphOut).
+insert_edges([],CFrom,CList,GraphIn,GraphOut):-
+        term_to_atom(adj-CFrom,Key),
+        GraphOut = GraphIn.put(Key,CList).
+
+test:-
+        Edges = [edge(a,d),edge(a,b),edge(b,c),edge(b,d), edge(c,d)],
+        edges_to_dict(Edges,GraphDict),
+        write(GraphDict),nl,
+        dfs_fronds(a,GraphDict,GraphDict2),
+        write(GraphDict2),nl.
+%%        simple_dot(First,DotString),
+%%        show_dot(DotString).
+
+%% DFS that assigns properties to edges: tree-edge or frond
+dfs_fronds(Vertex,GraphIn,GraphOut):-
+        dfs_fronds_rec(Vertex,[],_,GraphIn,GraphOut).
+dfs_fronds_rec(Vertex,AccIn,AccOut,GraphIn,GraphOut):-
+        term_to_atom(adj-Vertex,Key),
+        Adj = GraphIn.get(Key),
+        dfs_fronds_list(Vertex,Adj,[Vertex|AccIn],AccOut,GraphIn,GraphOut).
+dfs_fronds_list(Vertex,[H|T],AccIn,AccOut,GraphIn,GraphOut):-
+        member(H,AccIn),!,
+        term_to_atom(edge(H,Vertex),BackKey),
+        (   BackEdge = GraphIn.get(BackKey)
+        ->  EdgeType = BackEdge
+        ;   EdgeType = frond),
+        term_to_atom(edge(Vertex,H),Key),
+        Graph1 = GraphIn.put(Key,EdgeType),
+        dfs_fronds_list(Vertex,T,AccIn,AccOut,Graph1,GraphOut).
+dfs_fronds_list(Vertex,[H|T],AccIn,AccOut,GraphIn,GraphOut):-
+        nonmember(H,AccIn),!,
+        term_to_atom(edge(Vertex,H),Key),
+        Graph1 = GraphIn.put(Key,tree-edge),
+        dfs_fronds_rec(H,AccIn,Acc1,Graph1,Graph2),
+        dfs_fronds_list(Vertex,T,Acc1,AccOut,Graph2,GraphOut).
+dfs_fronds_list(_,[],Acc,Acc,Graph,Graph).
